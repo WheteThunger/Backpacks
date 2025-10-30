997a16f1519399b2b5799f9e4b77c55c4ba4696f
WhiteThunder <whetethunger@gmail.com>
2025-10-29 17:48:22 -0700

    Increment minor version

--- Backpacks.cs
+++ Backpacks.cs
@@ -27,7 +27,7 @@ using Time = UnityEngine.Time;
 
 namespace Oxide.Plugins
 {
-    [Info("Backpacks", "WhiteThunder", "3.16.1")]
+    [Info("Backpacks", "WhiteThunder", "3.17.0")]
     [Description("Allows players to have a Backpack which provides them extra inventory space.")]
     internal class Backpacks : CovalencePlugin
     {
